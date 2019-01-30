a3ec9ac25697a16b021ed527f3a0b658709f97c8
Thomas Hooper <tom@stainlessvision.com>
2019-01-30 00:03:34 +0000

    Update from pub

--- projects/clifford-bubble/shader.glsl
+++ projects/clifford-bubble/shader.glsl
@@ -5,51 +5,55 @@ precision mediump float;
 uniform float iTime;
 
 uniform vec2 iResolution;
+uniform vec4 iMouse;
 
-uniform mat4 projection;
-varying vec3 eye;
-varying vec3 dir;
-varying vec3 cameraForward;
+uniform float guiLead;
+uniform float guiRadius;
+
+void mainImage(out vec4 a, in vec2 b);
+
+void main() {
+    mainImage(gl_FragColor, gl_FragCoord.xy);
+}
+
+#ifdef GL_ES
+precision mediump float;
+#endif
 
 
 /* SHADERTOY FROM HERE */
+/*
 
-// #define DEBUG
+    Clifford Torus Rotation
+    -----------------------
+
+    Getting a good distance for this 4D stereographic projection was
+    tricky, see the notes in 'Main SDF', or just toggle DEBUG below to
+    see what's going on.
+
+    Big thanks to Matthew Arcus (mla) for providing a better torus
+    equation and improving the projection distance fix.
+
+    See also:
+
+    * Animation by Jason Hise https://www.youtube.com/watch?v=1_pzjvVixL0
+    * Clifford Torus by mla https://www.shadertoy.com/view/3ss3z4
+    * https://en.wikipedia.org/wiki/Clifford_torus
+    * http://virtualmathmuseum.org/Surface/clifford_torus/clifford_torus.html
+
+*/
+
+//#define DEBUG
+
+// --------------------------------------------------------
+// HG_SDF
+// https://www.shadertoy.com/view/Xs3GRB
+// --------------------------------------------------------
 
 #define PI 3.14159265359
 
 void pR(inout vec2 p, float a) {
     p = cos(a)*p + sin(a)*vec2(p.y, -p.x);
-}
-
-// Shortcut for 45-degrees rotation
-void pR45(inout vec2 p) {
-    p = (p + vec2(p.y, -p.x))*sqrt(0.5);
-}
-
-// http://www.neilmendoza.com/glsl-rotation-about-an-arbitrary-axis/
-mat3 rotationMatrix(vec3 axis, float angle)
-{
-    axis = normalize(axis);
-    float s = sin(angle);
-    float c = cos(angle);
-    float oc = 1.0 - c;
-    
-    return mat3(
-        oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,
-        oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,
-        oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c
-    );
-}
-
-
-// Repeat space along one axis. Use like this to repeat along the x axis:
-// <float cell = pMod1(p.x,5);> - using the return value is optional.
-float pMod1(inout float p, float size) {
-    float halfsize = size*0.5;
-    float c = floor((p + halfsize)/size);
-    p = mod(p + halfsize, size) - halfsize;
-    return c;
 }
 
 vec2 pMod2(inout vec2 p, vec2 size) {
@@ -58,39 +62,20 @@ vec2 pMod2(inout vec2 p, vec2 size) {
     return c;
 }
 
-vec3 pMod3(inout vec3 p, vec3 size) {
-    vec3 c = floor((p + size*0.5)/size);
-    p = mod(p + size*0.5, size) - size*0.5;
-    return c;
-}
-
-float vmax(vec2 v) {
-    return max(v.x, v.y);
-}
-
-float vmax(vec3 v) {
-    return max(max(v.x, v.y), v.z);
-}
-
-float fBox(vec2 p, vec2 b) {
-    vec2 d = abs(p) - b;
-    return length(max(d, vec2(0))) + vmax(min(d, vec2(0)));
-}
-
-float fBox(vec3 p, vec3 b) {
-    vec3 d = abs(p) - b;
-    return length(max(d, vec3(0))) + vmax(min(d, vec3(0)));
-}
-
-float smin(float a, float b, float r) {
-    vec2 u = max(vec2(r - a,r - b), vec2(0));
-    return max(r, min (a, b)) - length(u);
-}
-
 float smax(float a, float b, float r) {
     vec2 u = max(vec2(r + a,r + b), vec2(0));
     return min(-r, max (a, b)) + length(u);
 }
+
+float fTorus(vec3 p, float smallRadius, float largeRadius) {
+    return length(vec2(length(p.xz) - largeRadius, p.y)) - smallRadius;
+}
+
+
+// --------------------------------------------------------
+// Spectrum colour palette
+// IQ https://www.shadertoy.com/view/ll2GD3
+// --------------------------------------------------------
 
 vec3 pal( in float t, in vec3 a, in vec3 b, in vec3 c, in vec3 d ) {
     return a + b*cos( 6.28318*(c*t+d) );
@@ -101,23 +86,9 @@ vec3 spectrum(float n) {
 }
 
 
-// Torus in the XZ-plane
-float fTorus(vec3 p, float smallRadius, float largeRadius) {
-    return length(vec2(length(p.xz) - largeRadius, p.y)) - smallRadius;
-}
-
-void pModTorus(inout vec3 p, float smallRadius, float largeRadius) {
-    vec2 xy = vec2(length(p.xz), p.y) - vec2(largeRadius,0);
-    p = vec3(
-        (atan(p.x, p.z) / PI) * .5 + .5,
-        (atan(xy.y, xy.x) / PI) * .5 + .5,
-        fTorus(p, smallRadius, largeRadius)
-    );
-} 
-
-float time;
-vec3 mcolor;
-
+// --------------------------------------------------------
+// Main SDF
+// --------------------------------------------------------
 
 // Inverse stereographic projection of p,
 // p4 lies onto the unit 3-sphere centered at 0.
@@ -175,8 +146,9 @@ float fixDistance(float d, float k) {
     return d;
 }
 
+float time;
 vec2 modelUv;
-bool hit3DTorus = false;
+bool hitDebugTorus = false;
 
 float map(vec3 p) {
 
@@ -232,29 +204,6 @@ float map(vec3 p) {
 
     return d;
 
-    float n = sqrt(2.) * 20.;
-    float repeat = uvScale / n;
-
-    pR45(p.xy);
-
-    p.xy += repeat / 2.;
-
-    // p.z = abs(p.z) - .15;
-    // pMod1(p.z, .2);
-    pMod2(p.xy, vec2(repeat));
-
-    float ww = repeat * mix(.1, .6, smoothstep(3., 1., length(pp)));
-    // d = length(p) - ww;
-    d = fBox(p, vec3(ww,ww,ww*.5));
-
-    // ww = repeat * .01;
-    // d = fBox(p.xz, vec2(ww));
-    // d = min(d, fBox(p.yz, vec2(ww)));
-    // d = max(d, abs(p.z) - .1);
-    d = fixDistance(d, k);
-
-    // d = max(d, -d2);
-    return d;
 }
 
 bool hitDebugPlane = false;
@@ -266,6 +215,7 @@ float mapDebug(vec3 p) {
     #endif
     float plane = min(abs(p.z), abs(p.y));
     hitDebugPlane = plane < abs(d);
+    //hitDebugPlane = true;
     return hitDebugPlane ? plane : d;
 }
 
@@ -296,18 +246,12 @@ mat3 calcLookAtMatrix(vec3 ro, vec3 ta, vec3 up) {
     return mat3(uu, vv, ww);
 }
 
-void main() {
+void mainImage(out vec4 fragColor, in vec2 fragCoord) {
 
     time = mod(iTime / 2., 1.);
-    // time = .5;
-
-    vec3 rayOrigin = eye;
-    vec3 rayDirection = normalize(dir);
-    vec3 rayPosition = rayOrigin;
-    float rayLength = 0.;
-
-    float distance = 0.;
-    vec3 color = vec3(0);
+    #ifdef DEBUG
+        time = iTime / 6.;
+    #endif
 
     vec3 camPos = vec3(1.8, 5.5, -5.5) * 1.75;
     vec3 camTar = vec3(.0,0,.0);
@@ -315,10 +259,14 @@ void main() {
     mat3 camMat = calcLookAtMatrix(camPos, camTar, camUp);
     float focalLength = 5.;
     vec2 p = (-iResolution.xy + 2. * gl_FragCoord.xy) / iResolution.y;
-    rayDirection = normalize(camMat * vec3(p, focalLength));
-    rayOrigin = camPos;
-    rayPosition = rayOrigin;
 
+    vec3 rayDirection = normalize(camMat * vec3(p, focalLength));
+    vec3 rayOrigin = camPos;
+    vec3 rayPosition = rayOrigin;
+    float rayLength = 0.;
+
+    float distance = 0.;
+    vec3 color = vec3(0);
 
     float h, t;
     vec3 c;
@@ -373,5 +321,6 @@ void main() {
 
     // color = 1. -color;
     color = pow(color, vec3(1. / 2.2)); // Gamma
-    gl_FragColor = vec4(color, 1);
+
+    fragColor = vec4(color, 1);
 }
