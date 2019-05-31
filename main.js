b139d86ae2d4375a9948ea118c74f58f4f4d9942
Thomas Hooper <tom@stainlessvision.com>
2019-06-01 00:21:02 +0100

    Dependency mapping

--- main.js
+++ main.js
@@ -36,6 +36,59 @@ module.exports = (project) => {
 
   const canvas = regl._gl.canvas;
 
+  function findBuffers(shader) {
+    // Reads comment after uniform, e.g.
+    // uniform sampler2D iChannel0; // buffer-a.glsl, filter: linear, wrap: clamp
+    const dependencies = [];
+    const reUniform = /uniform sampler2D.*\/\/(.*)/g;
+    const reFile = /([^\s]+)\.glsl/;
+    const reFilter = /filter:\s*(\w+)/;
+    const reWrap = /wrap:\s*(\w+)/;
+    let match;
+    do {
+      match = reUniform.exec(shader);
+      if (match) {
+        dependencies.push({
+          name: match[1].match(reFile)[1],
+          filter: match[1].match(reFilter)[1],
+          wrap: match[1].match(reWrap)[1],
+        });
+      }
+    } while (match);
+    return dependencies;
+  }
+
+  const nodes = {};
+
+  Object.entries(project.shaders).forEach(([name, shader]) => {
+    nodes[name] = {
+      name,
+      shader,
+      dependencies: findBuffers(shader),
+    };
+  });
+
+  // https://www.electricmonk.nl/docs/dependency_resolving_algorithm/dependency_resolving_algorithm.html
+  function resolveDependency(node, resolved, unresolved) {
+    unresolved.push(node);
+    node.dependencies.forEach((dep) => {
+      const depNode = nodes[dep.name];
+      if (resolved.indexOf(depNode) === -1) {
+        if (unresolved.indexOf(depNode) !== -1) {
+          throw new Error(`Circular reference detected: ${node.name} -> ${depNode.name}`);
+        }
+        resolveDependency(depNode, resolved, unresolved);
+      }
+    });
+    resolved.push(node);
+    unresolved.push(node);
+  }
+
+  const renderOrder = [];
+  resolveDependency(nodes.main, renderOrder, []);
+  console.log(renderOrder);
+
+
   const setup = regl({
     uniforms: {
       projection: ({ viewportWidth, viewportHeight }) => mat4.perspective(
