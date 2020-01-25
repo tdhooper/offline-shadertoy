b239fd331e9fda8a4e65e24237bff6c5f0a39517
Thomas Hooper <tom@stainlessvision.com>
2020-01-25 15:35:27 +0000

    Remove unused

--- projects/bloom-skull/shader.glsl
+++ projects/bloom-skull/shader.glsl
@@ -12,48 +12,16 @@ void main() {
     mainImage(gl_FragColor, gl_FragCoord.xy);
 }
 
-// --------------------------------------------------------
-// Spectrum colour palette
-// IQ https://www.shadertoy.com/view/ll2GD3
-// --------------------------------------------------------
-
-vec3 pal( in float t, in vec3 a, in vec3 b, in vec3 c, in vec3 d ) {
-    return a + b*cos( 6.28318*(c*t+d) );
-}
-
-vec3 spectrum(float n) {
-    return pal( n, vec3(0.5,0.5,0.5),vec3(0.5,0.5,0.5),vec3(1.0,1.0,1.0),vec3(0.0,0.33,0.67) );
-}
-
-
-
 mat2 inverse(mat2 m) {
   return mat2(m[1][1],-m[0][1],
              -m[1][0], m[0][0]) / (m[0][0]*m[1][1] - m[0][1]*m[1][0]);
 }
 
-
 const float PI  = 3.14159265359;
 const float PHI = 1.61803398875;
 
-float vmax(vec3 v) {
-    return max(max(v.x, v.y), v.z);
-}
-
-// Box: correct distance to corners
-float fBox(vec3 p, vec3 b) {
-    vec3 d = abs(p) - b;
-    return length(max(d, vec3(0))) + vmax(min(d, vec3(0)));
-}
-
 void pR(inout vec2 p, float a) {
     p = cos(a)*p + sin(a)*vec2(p.y, -p.x);
-}
-
-
-float smax2(float a, float b, float r) {
-    vec2 u = max(vec2(r + a,r + b), vec2(0));
-    return min(-r, max (a, b)) + length(u);
 }
 
 float smin(float a, float b, float k){
@@ -72,21 +40,6 @@ float range(float vmin, float vmax, float value) {
 float rangec(float a, float b, float t) {
     return clamp(range(a, b, t), 0., 1.);
 }
-
-// Cylinder standing upright on the xz plane
-float fCylinder(vec3 p, float r, float height) {
-    float d = length(p.xz) - r;
-    d = max(d, abs(p.y) - height);
-    return d;
-}
-
-// Capsule: A Cylinder with round caps on both sides
-float fCapsule(vec3 p, float r, float c) {
-    c -= r;
-    // return fCylinder(p, r, c);
-    return mix(length(p.xz) - r, length(vec3(p.x, abs(p.y) - c, p.z)) - r, step(c, abs(p.y)));
-}
-
 
 vec2 cmul (vec2 a, vec2 b) {
   return vec2(
@@ -136,93 +89,15 @@ float circleFlatRadius(float x, float o) {
 
 float time;
 
-float bloom(vec3 p) {
-    float d = 1e12;
-    float part;
-    p.y -= .1;
-    vec3 pp = p;
-    float a, j;
-    vec3 s;
-    const float n = 40.;
-    for (float i = 1.; i < n; i ++) {
-        a = i * PHI * PI * 2.;
-        p = pp;
-        //p.xz -= vec2(sin(a), cos(a)) * i * .005;
-        j = pow(i/n, 1.5);
-        p.y += j * .2;
-        pR(p.xz, -a);
-        pR(p.yz, mix(.3, .8, i/n));
-        s = vec3(.05,.05,.01) * mix(.1, 1., i/n);
-        s.y = mix(.01, .15, i/n);
-        p.y -= s.y / 2.;
-        part = fBox(p, s - s.z) - s.z;
-        d = min(d, part);
-    }
-    return d;
-}
 
 vec2 round(vec2 a) {
     return floor(a + .5);
 }
 
-vec2 fib(vec2 n) {
-    vec2 uv = vec2(5., 8.);
-    float aa = atan(uv.x / uv.y);
-    float r = (PI*2.) / sqrt(uv.x*uv.x + uv.y*uv.y);
-    //n.y = min(n.y, 1.);
-    //n.y = max(n.y, .7);
-    pR(n, aa);
-    vec2 cell = round(n / r);
-    n = cell * r;    
-    pR(n, -aa);
-
-    return n;
-}
-
 float bloomHeight = 1.;
 float bloomHeightMax = 1.;
 
-float leafBound(vec3 p, vec2 uv) {
-    float d = abs(length(p) - uv.y) - .16;
-    //return d;
-    //d = max(d, p.y);
-    pR(p.xz, -uv.x);
-   // vec3 n = normalize(vec3(1,0,.5));
-    
-    float width = mix(.5, .1, min(uv.y, 1.));
-    width = .75 / uv.y;
-    vec3 n = normalize(vec3(1,0,width));
-
-    d = max(d, -dot(p, n));
-    d = max(d, dot(p, n * vec3(1,1,-1)));
-    return d;
-}
-
-float debugLeaf(vec3 p, vec2 uv) {
-
-    float bound = length(p + vec3(0, bloomHeight, 0)) - bloomHeight * 3.;
-
-    //return bound;
-
-    float r = circleFlatRadius(uv.y, bloomHeight);
-    if (r == 0.) {
-        return 1e12;
-    }
-    p.y += uv.y - r;
-    float d2 = abs(length(p) - abs(r)) - .05;
-
-    float d = abs(length(p) - uv.y) - .01;
-    // d = min(d, d2);
-    d = d2;
-    d = max(d, bound);
-    pR(p.xz, -uv.x);
-    d = max(d, abs(p.x) - .05);
-
-
-    return d;
-}
-
-float newLeaf(vec3 p, vec2 uv) {
+float leaf(vec3 p, vec2 uv) {
     float r = circleFlatRadius(uv.y, bloomHeight);
     if (r == 0.) {
         return 1e12;
@@ -264,32 +139,6 @@ float newLeaf(vec3 p, vec2 uv) {
     return d;
 }
 
-float leaf(vec3 p, vec2 uv) {
-   return newLeaf(p, uv);
-
-    float thick = clamp(uv.y, .7, 1.);
-    thick = 1.;
-    float th = thick * .16;
-    pR(p.xz, -uv.x);
-    //d = max(d, abs(p.x) - .05);
-    float width = mix(.5, .1, min(uv.y, 1.));
-    width = .75 / uv.y;
-    width *= thick;
-    vec3 n = normalize(vec3(1,0,width));
-    float d = -dot(p, n);
-    d = max(d, dot(p, n * vec3(1,1,-1)));
-    float len = mix(PI / 1.2, PI / 2., pow(uv.y/2.9, 2.));
-    len = max(len, 0.);
-    d = smax(d, dot(p, vec3(0,sin(len),cos(len))), thick);
-    d = smax(d, abs(length(p) - uv.y) - thick * th, th);
-    //vec3 n = vec3(sin(uv.x + PI/2.), 0, cos(uv.x + PI/2.));
-    //d = max(d, abs(dot(p, n)) - .05);
-    p.z -= uv.y;
-    //d = min(d, length(p) - .1);
-    return d;
-}
-
-
 vec2 calcCell(
     vec2 cell,
     vec2 offset,
@@ -314,27 +163,17 @@ vec2 calcCell(
 
 vec3 bloom2(vec3 p) {
 
-    float bound = length(p - vec3(0,-1.2,0)) - 3.3;
-    bound = max(bound, p.y - 1.1);
-    // bound = length(p + vec3(0, bloomHeight, 0)) - bloomHeight/1.;
-    if (bound > .01) {
-        // return vec2(bound, 0.);
-    }
-
     float t = iTime / 5.1;
     t = mod(t, 1.);
     t = smoothstep(0., .7, t) - pow(rangec(.7, 1., t), 2.);
-    //t *= 1.5;
+    t *= 1.5;
     float ts = smoothstep(.6, 1.1, t);
     bloomHeight = mix(.1, bloomHeightMax, t);
     p.y -= bloomHeight;
 
     float skull = length(p) - .4 * ts;
 
-    //bloomHeight = bloomHeightMax;
-    // t = 2.;
-    //t -= .05;
-    vec2 move = vec2(0, t)* bloomHeightMax;
+    vec2 move = vec2(0, t) * bloomHeightMax;
     float stretch = 5.;
 
     // move *= 0.;
@@ -363,16 +202,6 @@ vec3 bloom2(vec3 p) {
     uv = transform * uv;
     vec2 cell = round(uv / scale);
 
-    // cell.x = min(cell.x, 1.);
-
-    //bound = leafBound(p, ((cell + vec2(0, 0)) * transform * scale) + move);
-    //if (bound > .01) {
-    //    return bound;
-    //}
-
-    vec2 ocell = cell;
-    // cell *= 0.;
-
     float d = 1e12;
 
     d = min(d, leaf(p, calcCell(cell, vec2(-1, 0), transform, transformI, scale, move, stretch)));
@@ -387,15 +216,6 @@ vec3 bloom2(vec3 p) {
     d = min(d, leaf(p, calcCell(cell, vec2(1, 1), transform, transformI, scale, move, stretch)));
 
     d = min(d, skull);
-
-cell = ocell;
-    
-    // float b = length(p.xz) - .2;
-    // b = fCapsule(p + vec3(0,bloomHeight,0), 1., bloomHeight);
-    // if (b < d) {
-    //     float c = cell.x / 5. + cell.y / 2.;
-    //     return vec3(b, c, 1.);
-    // }
 
     return vec3(d, 0, 0);
 }
@@ -486,10 +306,6 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
         if ( ! bg) {
             vec3 nor = calcNormal(rayPosition);
             col = nor * .5 + .5;
-            if (res.z == 1.) {
-                col = spectrum(res.y);
-                col *= clamp(dot(nor, vec3(1,1,0)), 0., 1.) * .5 + .5;
-            }
         }
 
         tot += col;
