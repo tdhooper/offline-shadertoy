35361e6537810e73242ef3da403f404180c740d6
Thomas Hooper <thomash@last.fm>
2020-03-07 22:46:05 +0000

    Mirror skull

--- projects/skull/draw.js
+++ projects/skull/draw.js
@@ -11,14 +11,18 @@ const model = mat4.create();
 // mat4.rotateY(model, model, .56);
 // mat4.rotateZ(model, model, .01);
 mat4.scale(model, model, [.55,.55,.55]);
-mat4.translate(model, model, [.12,.6,0]);
+mat4.translate(model, model, [.11,.6,0]);
 mat4.rotateX(model, model, Math.PI / 2.);
-mat4.rotateY(model, model, -.02);
+mat4.rotateY(model, model, -.05);
 
+const model2 = mat4.create();
+mat4.scale(model2, model2, [-1,1,1]);
+mat4.multiply(model2, model2, model);
 
 function init(drawRaymarch, renderNodes, uniforms) {
   const uu = Object.assign({}, uniforms);
-  uu.model = model;
+  uu.model = regl.prop('model');
+  uu.albedo = regl.prop('albedo');
 
   const buffer = regl.framebuffer({
     width: 1024,
@@ -42,13 +46,14 @@ function init(drawRaymarch, renderNodes, uniforms) {
       #extension GL_OES_standard_derivatives : enable
       precision mediump float;
       uniform bool guiSplit;
+      uniform vec3 albedo;
       varying vec3 vnormal;
       void main() {
         vec3 color = vnormal * .5 + .5;
         vec3 lig = vec3(0,1.5,.5);
         lig = normalize(vec3(1,1,0));
         // if ( ! guiSplit) {
-          color = vec3(1) * pow(clamp(dot(lig, vnormal) * .5 + .5, 0., 1.), 1./2.2);
+          color = albedo * pow(clamp(dot(lig, vnormal) * .5 + .5, 0., 1.), 1./2.2);
         // }
         gl_FragColor = vec4(color, 1);
       }
@@ -83,7 +88,9 @@ function init(drawRaymarch, renderNodes, uniforms) {
       buffer.resize(context.viewportWidth, context.viewportHeight);
     }
 
-    drawPolygons(state);
+    drawPolygons(Object.assign({model: model, albedo: [1,1,1]}, state));
+    drawPolygons(Object.assign({model: model2, albedo: [1,1,1]}, state));
+
     setup(state, (context) => {
       drawRaymarch(state, () => {
         renderNodes.forEach((node) => {
