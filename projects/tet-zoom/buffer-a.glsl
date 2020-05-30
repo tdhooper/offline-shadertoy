769cde337ab70e4000ec6c0649250f959ccf0e73
Thomas Hooper <tom@stainlessvision.com>
2020-05-30 03:12:58 +0100

    wip dark

--- projects/tet-zoom/buffer-a.glsl
+++ projects/tet-zoom/buffer-a.glsl
@@ -343,8 +343,12 @@ float intersectPlane(vec3 rOrigin, vec3 rayDir, vec3 origin, vec3 normal, vec3 u
 }
 
 vec3 light(vec3 origin, vec3 rayDir) {
+    //origin.z *= -1.;
+    //rayDir.z *= -1.;
     origin = -(cameraMatrix * vec4(origin, 1)).xyz;
     rayDir = -(cameraMatrix * vec4(rayDir, 0)).xyz;
+
+    pR(rayDir.yz, 1.5);    
 
     vec2 uv;
     float hit = intersectPlane(origin, rayDir, vec3(5,-2,-8), normalize(vec3(1,-.5,-.1)), normalize(vec3(0,1,0)), uv);
@@ -353,10 +357,21 @@ vec3 light(vec3 origin, vec3 rayDir) {
 }
 
 vec3 env(vec3 origin, vec3 rayDir) {
+    //origin.xz *= -1.;
+    //rayDir.xz *= -1.;
+    
     origin = -(cameraMatrix * vec4(origin, 1)).xyz;
     rayDir = -(cameraMatrix * vec4(rayDir, 0)).xyz;
 
+    //pR(rayDir.xy, -1.);
+    //pR(rayDir.yz, 1.5);    
+
+    pR(rayDir.yz, 1.5);    
+
+
     float l = smoothstep(.0, 1.7, dot(rayDir, vec3(.5,-.3,1))) * .4;
+    //l = smoothstep(-.5, 2.2, dot(rayDir, vec3(.5,-.3,1))) * .4;
+    //l = smoothstep(.2, .5, dot(rayDir, vec3(.5,-.3,1))) * .4;
    	return vec3(l) * vec3(1,1,1);
 }
 
@@ -367,7 +382,7 @@ vec3 env(vec3 origin, vec3 rayDir) {
 //========================================================
 
 const float MAX_DISPERSE = 5.;
-const float MAX_BOUNCE = 10.;
+const float MAX_BOUNCE = 15.;
 
 vec3 normal(in vec3 p){
   vec3 v = vec3(.001, 0, 0);
@@ -412,12 +427,102 @@ Hit march(vec3 origin, vec3 rayDir, float invert, float maxDist) {
 #pragma glslify: distanceMeter = require(../clifford-torus/distance-meter.glsl)
 
 
+
+
+
+
+
+#define M_PI 3.14159
+#define drawSphere true
+
+const float radius = 0.3;
+
+float angVelocity = -0.3,
+	  phi = 0.0,
+      psi = 0.0,
+      theta = 0.0;
+
+
+vec3 sphericalToWorld(vec2 sphCoord, float r)
+{
+    return vec3(
+    	r * sin(sphCoord.y) * cos(sphCoord.x),
+        r * sin(sphCoord.y) * sin(sphCoord.x),
+        r * cos(sphCoord.y)
+    );
+}
+
+vec2 worldToSpherical(vec3 flatCoord, float r)
+{
+    return vec2(
+        atan(flatCoord.x, flatCoord.y),
+        acos(flatCoord.z / r)
+    );   
+}
+ 
+mat3 makeRotationMatrix(vec3 a)
+{
+    return mat3(
+    	cos(a.x) * cos(a.z) - sin(a.x) * cos(a.y) * sin(a.z),
+        -cos(a.x) * sin(a.z) - sin(a.x) * cos(a.y) * cos(a.z),
+        sin(a.x) * sin(a.y),
+        sin(a.x) * cos(a.z) + cos(a.x) * cos(a.y) * sin(a.z),
+        -sin(a.x) * sin(a.z) + cos(a.x) * cos(a.y) * cos(a.z),
+        -cos(a.x) * sin(a.y),
+        sin(a.y) * sin(a.z),
+        sin(a.y) * cos(a.z),
+        cos(a.y)
+    );
+}
+
+vec3 screenToWorld(vec2 myPos, vec2 sphereCenter, float r)
+{
+    vec3 myVec;
+    myVec.y = myPos.x - sphereCenter.x;
+    myVec.z = -(myPos.y - sphereCenter.y);
+    myVec.x = sqrt(r * r - myVec.z * myVec.z - myVec.y * myVec.y);
+    return myVec;
+}
+
+void XmainImage(out vec4 fragColor, vec2 fragCoord)
+{
+    vec2 mouse = iMouse.xy;
+  	phi = .5;
+    psi = .5;
+    theta = iTime * angVelocity;
+    
+    vec2 sphCenter = .5*vec2(iResolution.x/iResolution.y, 1.);
+    vec2 p = fragCoord.xy / iResolution.xy;
+ 
+    vec3 worldSphCoord = sphericalToWorld(p * vec2(2.0 * M_PI, M_PI), 1.0);
+    
+    p.x *= iResolution.x / iResolution.y;
+    
+    if (drawSphere && length(p - sphCenter) < radius)
+        worldSphCoord = screenToWorld(p, sphCenter, radius);
+    
+    mat3 rotationMatrix = makeRotationMatrix(vec3(phi, psi, theta));
+    vec3 rotatedWorldSphCoord = normalize(rotationMatrix * worldSphCoord);
+
+    fragColor = vec4(env(vec3(0), rotatedWorldSphCoord), 1.);
+
+    //vec2 rotatedSphericalCoord = worldToSpherical(rotatedWorldSphCoord, 1.0);
+
+    //fragColor = texture(iChannel0, rotatedSphericalCoord / vec2(2.*M_PI, M_PI), 0.0);
+}
+
+
+
+
+
+
+
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     initPoly();
 
     time = mod(iTime / 2., 1.);
-    //time = fract(iTime);
+    time = fract(iTime);
     time = fract(time + .36);
     
     vec2 uv = (2. * fragCoord - iResolution.xy) / iResolution.y;
@@ -467,7 +572,7 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
 
             if ( res.y == 0. || bounce == MAX_BOUNCE - 1.) {
                 if (bounce == 0.) {
-                	sam += bgCol; break;	
+                //	sam += bgCol; break;	
                 }
                 sam += env(origin, rayDir);
                 break;
