54d83eb9ec54979d24215e08709d824eaf84309d
tdhooper <projects@stainlessvision.com>
2024-09-30 16:39:11 +0100

    Groundwok for finding gizmo transform

--- projects/gizmo/draw.js
+++ projects/gizmo/draw.js
@@ -1,5 +1,6 @@
 const createCube = require('primitive-cube');
 const { mat4 } = require('gl-matrix');
+const glslify = require('glslify');
 
 function offsetCells(cells, offset) {
   return cells.map(function(cell) {
@@ -38,7 +39,17 @@ const mesh = mergeMeshes([
   createCube(.1, .1, 2),
 ]);
 
+const model = mat4.create();
+mat4.rotateX(model, model, -.38);
+mat4.rotateY(model, model, .56);
+mat4.rotateZ(model, model, .01);
+mat4.translate(model, model, [.222,-.5,.15]);
+//mat4.scale(model, model, [50, 50, 50]);
+
 const createDraw = function(uniforms, setupProjectionView) {
+
+  const polyUniforms = Object.assign({}, uniforms);
+  polyUniforms.model = model;
 
   const drawPolygons = global.regl({
     vert: `
@@ -63,13 +74,53 @@ const createDraw = function(uniforms, setupProjectionView) {
       normal: mesh.normals,
     },
     elements: mesh.cells,
-    uniforms: uniforms,
+    uniforms: polyUniforms,
   });
+
+  const evalPositions = [
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1],
+    [10, 11, 12],
+    [13, 14, 15],
+    [16, 17, 18],
+  ];
+
+  const evalGizmoResults = regl.framebuffer({
+    width: evalPositions.length,
+    height: 1,
+    colorType: 'float',
+  }); 
+  
+  const drawEvalGizmo = regl({
+    vert: glslify('./eval-gizmo.vert'),
+    frag: glslify('./eval-gizmo.frag'),
+    attributes: {
+      position: evalPositions,
+      index: Array(evalPositions.length).fill().map((x, i) => i),
+    },
+    count: evalPositions.length,
+    primitive: 'points',
+    uniforms: {
+      count: evalPositions.length,
+    },
+    framebuffer: evalGizmoResults,
+  });
+
+  uniforms['evalGizmoResults'] = evalGizmoResults;
 
   return function draw(state, drawShader) {
     setupProjectionView(state, (context) => {
       drawPolygons(state);
     });
+    drawEvalGizmo();
+
+    var pixels = regl.read({
+      framebuffer: evalGizmoResults
+    })
+
+    console.log(pixels);
+
     drawShader();
   };
 };
--- projects/gizmo/map.glsl
+++ projects/gizmo/map.glsl
@@ -1,5 +1,15 @@
+vec3 GIZMO_LOCAL_P;
+
+void GIZMO(vec3 p) {
+    GIZMO_LOCAL_P = p;
+}
 
 float map(vec3 p) {
+
+    p -= 1.;
+
+    GIZMO(p);
+
     return length(p) - .5;
 }
 
