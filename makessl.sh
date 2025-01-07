e1bf41d8002068c1ab015e7a2f8dd462f9f894f4
Thomas Hooper <tom@stainlessvision.com>
2025-01-07 10:27:56 +0000

    makessl

---
+++ makessl.sh
@@ -0,0 +1,5 @@
+mkdir -p ssl;
+openssl req -x509 -out ssl/certificate.pem -keyout ssl/private-key.pem \
+  -newkey rsa:2048 -nodes -sha256 \
+  -subj '/CN=localhost' -extensions EXT -config <( \
+   printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
