691a4a8f7bbe50f093c3c0e1d2df6f357d21b982
tdhooper <projects@stainlessvision.com>
2025-01-18 12:25:09 +0000

    Debug safari issue

--- projects/blank-webgl2/shader.glsl
+++ projects/blank-webgl2/shader.glsl
@@ -4,6 +4,12 @@ precision highp float;
 
 out vec4 fragColor;
 
+void mainImage(out vec4 a, in vec2 b);
+
 void main() {
+    mainImage(fragColor, gl_FragCoord.xy);
+}
+
+void mainImage(out vec4 fragColor, in vec2 fragCoord) {
   fragColor = vec4(1,0,0,1);
 }
