8ebe9d5da9cec18db4a2ad0f2b67ef2c7616793c
Thomas Hooper <tom@stainlessvision.com>
2025-02-05 19:51:38 +0000

    pbrish

---
+++ projects/pbrish/buffer-a.glsl
@@ -0,0 +1,469 @@
+#version 300 es
+
+// framebuffer drawcount: 1
+
+precision highp float;
+
+uniform vec2 iResolution;
+uniform mat4 cameraViewMatrix;
+uniform sampler2D previousSample; // buffer-a.glsl filter: linear
+uniform float drawIndex;
+uniform int iFrame;
+uniform float iTime;
+uniform vec4 iMouse;
+
+in vec3 eye;
+in vec3 dir;
+in float fov;
+in float aspect;
+in mat4 vView;
+
+out vec4 fragColorOut;
+
+void mainImage(out vec4 a, in vec2 b);
+
+void main() {
+    mainImage(fragColorOut, gl_FragCoord.xy);
+}
+
+// Dave_Hoskins https://www.shadertoy.com/view/4djSRW
+vec2 hash22(vec2 p)
+{
+    p += 1.61803398875; // fix artifacts when reseeding
+	vec3 p3 = fract(vec3(p.xyx) * vec3(.1031, .1030, .0973));
+    p3 += dot(p3, p3.yzx+33.33);
+    return fract((p3.xx+p3.yz)*p3.zy);
+}
+
+// Dave_Hoskins https://www.shadertoy.com/view/4djSRW
+float hash12(vec2 p)
+{
+	vec3 p3  = fract(vec3(p.xyx) * .1031);
+    p3 += dot(p3, p3.yzx + 33.33);
+    return fract((p3.x + p3.y) * p3.z);
+}
+
+// iq https://www.shadertoy.com/view/tl23Rm
+vec2 rndunit2(vec2 seed ) {
+    vec2 h = seed * vec2(1,6.28318530718);
+    float phi = h.y;
+    float r = sqrt(h.x);
+	return r*vec2(sin(phi),cos(phi));
+}
+
+#define PI 3.14159265359
+
+void pR(inout vec2 p, float a) {
+    p = cos(a)*p + sin(a)*vec2(p.y, -p.x);
+}
+
+float smin(float a, float b, float k){
+    float f = clamp(0.5 + 0.5 * ((a - b) / k), 0., 1.);
+    return (1. - f) * a + f  * b - f * (1. - f) * k;
+}
+
+float smax(float a, float b, float k) {
+    return -smin(-a, -b, k);
+}
+
+float vmax(vec2 v) {
+	return max(v.x, v.y);
+}
+
+float vmin(vec2 v) {
+	return min(v.x, v.y);
+}
+
+float vmax(vec3 v) {
+	return max(max(v.x, v.y), v.z);
+}
+
+float vmin(vec3 v) {
+	return min(min(v.x, v.y), v.z);
+}
+
+float fBox(vec2 p, vec2 b) {
+	vec2 d = abs(p) - b;
+	return length(max(d, vec2(0))) + vmax(min(d, vec2(0)));
+}
+
+float fBox(vec3 p, vec3 b) {
+	vec3 d = abs(p) - b;
+	return length(max(d, vec3(0))) + vmax(min(d, vec3(0)));
+}
+
+float time;
+
+float spaceAnimFreq = .06;
+float startScale = 1.5;
+
+vec3 animAmp = vec3(-.02,.02,-.06) * 3.;
+ 
+struct Material {
+    vec3 albedo;
+    vec3 specularColor;
+    float roughness;
+    float metalness;
+};
+
+struct Model {
+    float d;
+    vec3 uvw;
+    int id;
+};
+
+Material shadeModel(Model model, inout vec3 nor) {
+    
+    vec3 albedo = vec3(.5);
+    vec3 specularColor = vec3(1);
+
+    if (model.uvw.z == 5.) albedo = vec3(.5,0,0);
+    if (model.uvw.z == 4.) albedo = vec3(0,.5,0);
+    if (model.uvw.z == 3.) albedo = vec3(0,0,.5);
+
+    if (model.uvw.z == 5.) specularColor = vec3(1,0,0);
+    if (model.uvw.z == 4.) specularColor = vec3(0,1,0);
+    if (model.uvw.z == 3.) specularColor = vec3(0,0,1);
+
+    float roughness = model.uvw.x / 3.;
+    float metalness = model.uvw.y / 3.;
+
+    return Material(albedo, specularColor, roughness, metalness);
+}
+
+Model map(vec3 p) {
+
+    vec3 count = vec3(4,4,7);
+
+    p += (count - 1.) / 2.;
+
+    vec3 c = floor(p + .5);
+
+    c = clamp(c, vec3(0), count - 1.);
+
+    p -= c;
+
+    float d = length(p) - .3;
+
+    return Model(d, c, 1);
+
+}
+
+
+//========================================================
+// Rendering
+//========================================================
+
+vec3 calcNormal( in vec3 p ) // for function f(p)
+{
+    const float eps = 0.0001; // or some other value
+    const vec2 h = vec2(eps,0);
+    return normalize( vec3(map(p+h.xyy).d - map(p-h.xyy).d,
+                           map(p+h.yxy).d - map(p-h.yxy).d,
+                           map(p+h.yyx).d - map(p-h.yyx).d ) );
+}
+
+
+vec3 sunPos = normalize(vec3(-.5,.5,-.25)) * 100.;
+vec3 skyColor = vec3(0.50,0.70,1.00);
+vec3 sunColor = vec3(8.10,6.00,4.20) * 4.5;
+
+
+vec3 env(vec3 dir) {
+    vec3 col = mix(vec3(.5,.7,1) * .0, vec3(.5,.7,1) * 1., smoothstep(-.2, .2, dir.y));
+    return col * .6;
+}
+
+struct Hit {
+    Model model;
+    vec3 pos;
+};
+
+float closestRayLen;
+
+Hit march(vec3 origin, vec3 rayDirection, float maxDist, float understep) {
+
+    float mind = 1e12;
+    vec3 rayPosition;
+    float rayLength, dist = 0.;
+    Model model;
+
+    for (int i = 0; i < 400; i++) {
+        rayPosition = origin + rayDirection * rayLength;
+        model = map(rayPosition);
+        rayLength += model.d * understep;
+
+        if (model.d < mind)
+        {
+            mind = model.d;
+            closestRayLen = rayLength;
+        }
+
+        if (model.d < .0002) break;
+
+        if (rayLength > maxDist) {
+            model.id = 0;
+            break;
+        }
+    }
+    return Hit(model, rayPosition);
+}
+
+
+// tracing/lighting setup from yx
+// https://www.shadertoy.com/view/ts2cWm
+vec3 ortho(vec3 a){
+    vec3 b=cross(vec3(-1,-1,.5),a);
+    // assume b is nonzero
+    return (b);
+}
+
+// re-borrowed from yx from
+// http://blog.hvidtfeldts.net/index.php/2015/01/path-tracing-3d-fractals/
+vec3 getSampleBiased(vec3 dir, float power, vec2 seed) {
+	dir = normalize(dir);
+	vec3 o1 = normalize(ortho(dir));
+	vec3 o2 = normalize(cross(dir, o1));
+	vec2 r = seed;
+	r.x=r.x*2.*PI;
+	r.y=pow(r.y,1.0/(power+1.0));
+	float oneminus = sqrt(1.0-r.y*r.y);
+	return cos(r.x)*oneminus*o1+sin(r.x)*oneminus*o2+r.y*dir;
+}
+
+vec3 getConeSample(vec3 dir, float extent, vec2 seed) {
+	dir = normalize(dir);
+	vec3 o1 = normalize(ortho(dir));
+	vec3 o2 = normalize(cross(dir, o1));
+	vec2 r =  seed;
+	r.x=r.x*2.*PI;
+	r.y=1.0-r.y*extent;
+	float oneminus = sqrt(1.0-r.y*r.y);
+	return cos(r.x)*oneminus*o1+sin(r.x)*oneminus*o2+r.y*dir;
+}
+
+vec3 sampleDirect(Hit hit, vec3 nor, vec3 throughput, inout vec2 seed) {
+    vec3 col = vec3(0);
+    vec3 lightDir = (sunPos - hit.pos);
+    vec3 lightSampleDir = getConeSample(lightDir, .0005, seed);
+    seed = hash22(seed);
+    float diffuse = dot(nor, lightSampleDir);
+    vec3 shadowOrigin = hit.pos + nor * (.0002 / abs(dot(lightSampleDir, nor)));
+    if (diffuse > 0.) {
+        Hit sh = march(shadowOrigin, lightSampleDir, 1., 1.);
+        if (sh.model.id == 0) {
+            col += throughput * sunColor/10. * diffuse;
+        }
+    }
+    return col;
+}
+
+float G1V(float dnv, float k){
+    return 1.0/(dnv*(1.0-k)+k);
+}
+
+// noby https://www.shadertoy.com/view/lllBDM
+float ggx(vec3 nor, vec3 rayDir, vec3 l, float rough, float f0){
+    float alpha = rough*rough;
+    vec3 h = normalize(-rayDir + l);
+    float dnl = clamp(dot(nor,l), 0.0, 1.0);
+    float dnv = clamp(dot(nor,rayDir), 0.0, 1.0);
+    float dnh = clamp(dot(nor,h), 0.0, 1.0);
+    float dlh = clamp(dot(l,h), 0.0, 1.0);
+    float f, d, vis;
+    float asqr = alpha*alpha;
+    const float pi = 3.14159;
+    float den = dnh*dnh*(asqr-1.0)+1.0;
+    d = asqr/(pi * den * den);
+    dlh = pow(1.0-dlh, 5.0);
+    f = f0 + (1.0-f0)*dlh;
+    float k = alpha/1.0;
+    vis = G1V(dnl, k)*G1V(dnv, k);
+    float spec = dnl * d * f * vis;
+    return spec;
+}
+
+vec3 sphereLight(vec3 lightPos, float radius, vec3 pos, vec3 rayDir, vec3 nor) {
+    vec3 L = (lightPos - pos);
+    vec3 ref = reflect(rayDir, nor);
+    vec3 centerToRay = dot(L, ref) * ref - L;
+    vec3 closestPoint = L + centerToRay * clamp(radius / length(centerToRay), 0., 1.);
+    return closestPoint;
+}
+
+vec3 sampleDirectSpec(Hit hit, vec3 rayDir, vec3 nor, float rough, inout vec2 seed) {
+    vec3 lpos = sphereLight(sunPos, 5., hit.pos, rayDir, nor);
+    
+    vec3 lightDir = normalize(lpos - hit.pos);
+    vec3 h = normalize(rayDir + lightDir);
+    float specular = pow(clamp(dot(h, nor), 0., 1.), 64.0);
+
+    vec3 col = vec3(0);
+
+    float fresnel = pow(max(0., 1. + dot(nor, rayDir)), 5.);
+    specular = ggx(nor, rayDir, lightDir, rough, fresnel);
+
+    vec3 shadowOrigin = hit.pos + nor * (.0002 / abs(dot(lightDir, nor)));
+    if (specular > 0.) {
+        Hit sh = march(shadowOrigin, lightDir, 1., 1.);
+        if (sh.model.id == 0) {
+            col += sunColor * specular * .1;
+        }
+    }
+    return col;
+}
+
+void getCamera(out vec3 origin, out vec3 rayDir, vec2 seed, float coc) {
+    origin = eye;
+    rayDir = normalize(dir);
+
+    vec3 cameraForward = -transpose(vView)[2].xyz;
+
+    // position on screen
+    vec3 p = origin + rayDir / dot(rayDir, cameraForward) / fov;
+
+    // jitter for antialiasing
+    p += vec3(2. * (seed - .5) / iResolution.y, 0) * mat3(vView);
+    rayDir = normalize(p - origin);
+
+    #ifdef DOF
+
+    // position on focal plane
+    Hit dofHit = march(origin, cameraForward, 100., 1.);
+    //float focalDistance = length(origin - dofHit.pos);
+    float focalDistance = closestRayLen;
+    vec3 focalPlanePosition = origin + rayDir / dot(rayDir, cameraForward) * focalDistance;
+    
+    // position on sensor plane
+    vec3 sensorPlanePosition = origin - (p - origin);
+    sensorPlanePosition += vec3(rndunit2(seed), 0.) * mat3(vView) * coc;
+
+    rayDir = normalize(focalPlanePosition - sensorPlanePosition);
+    origin = sensorPlanePosition + rayDir / dot(rayDir, cameraForward) / fov;
+
+    #else
+
+    origin = p - rayDir / dot(rayDir, cameraForward) / fov;
+
+    #endif
+}
+
+
+const float sqrt3 = 1.7320508075688772;
+
+// main path tracing loop, based on yx's
+// https://www.shadertoy.com/view/ts2cWm
+// with a bit of demofox's
+// https://www.shadertoy.com/view/WsBBR3
+vec4 draw(vec2 fragCoord, int frame) {
+
+    vec2 p = (-iResolution.xy + 2.* fragCoord) / iResolution.y;
+    
+    //p *= .85;
+
+    vec2 seed = hash22(fragCoord + (float(frame)) * sqrt3);
+    
+    // jitter for antialiasing
+    p += 2. * (seed - .5) / iResolution.xy;
+
+    vec3 col = vec3(0);
+
+    float focalLength = 6.;
+    vec3 camPos = vec3(0,0,.4) * focalLength * 1.;
+    vec3 camTar = vec3(0);
+    
+    vec3 ww = normalize(camTar - camPos);
+    vec3 uu = normalize(cross(vec3(0,1,0),ww));
+    vec3 vv = normalize(cross(ww,uu));
+    mat3 camMat = mat3(-uu, vv, ww);
+    
+    vec3 rayDir = normalize(camMat * vec3(p.xy, focalLength));
+    vec3 origin = camPos;
+
+    getCamera(origin, rayDir, seed, .5);
+
+    //vec3 origin = eye;
+    //vec3 rayDir = normalize(vec3(p.x * fov, p.y * fov, -1.) * mat3(vView));
+
+/*
+    #ifdef DOF
+    float fpd = .36 * focalLength;
+    vec3 fp = origin + rayDir * fpd;
+    origin = origin + camMat * vec3(rndunit2(seed), 0.) * .02;
+    rayDir = normalize(fp - origin);
+    #endif
+*/
+    Hit hit;
+    vec3 nor, ref;
+    Material material;
+    vec3 throughput = vec3(1);
+    vec3 bgCol = skyColor;
+
+    const int MAX_BOUNCE = 6;
+    
+    for (int bounce = 0; bounce < MAX_BOUNCE; bounce++) {
+   
+        hit = march(origin, rayDir, 20., 1.);
+   
+        if (hit.model.id == 0)
+        {
+            col += env(rayDir) * throughput;
+            break;
+        }
+
+        nor = calcNormal(hit.pos);
+        material = shadeModel(hit.model, nor);
+
+        // calculate whether we are going to do a diffuse or specular reflection ray 
+        seed = hash22(seed);
+        // doSpecular = hash12(seed) < material.specular;
+
+        // if ( ! doSpecular) {
+        //     // update the colorMultiplier
+            throughput *= material.albedo;
+        // }
+
+        // // Calculate diffuse ray direction
+        // seed = hash22(seed);
+        vec3 diffuseRayDir = getSampleBiased(nor, 1., seed);
+
+        // if ( ! doSpecular)
+        // {
+            seed = hash22(seed);
+            col += sampleDirect(hit, nor, throughput, seed);
+            rayDir = diffuseRayDir;
+        // }
+        // else
+        // {
+        //     if (bounce == 0) { // fix fireflies from diffuse-bounce specular
+        //         seed = hash22(seed);
+        //         col += sampleDirectSpec(hit, rayDir, nor, material.roughness, seed) * throughput;
+        //     }
+            
+        //     // Calculate specular ray direction
+        //     vec3 specularRayDir = reflect(rayDir, nor);
+        //     rayDir = normalize(mix(specularRayDir, diffuseRayDir, material.roughness * material.roughness));
+        // }
+
+        // offset from sufrace https://www.shadertoy.com/view/lsXGzH
+        origin = hit.pos + nor * (.0002 / abs(dot(rayDir, nor)));
+    }
+
+    return vec4(col, 1);
+}
+
+void mainImage(out vec4 fragColor, in vec2 fragCoord) {
+    time = fract(iTime / 6.);
+    
+    time = fract(time + 0.5);
+
+
+    vec4 col = draw(fragCoord, iFrame);
+   
+    if (drawIndex > 0.) {
+        vec4 lastCol = texture(previousSample, fragCoord.xy / iResolution.xy);
+        col = mix(lastCol, col, 1. / (drawIndex + 1.));
+    }
+    
+    fragColor = vec4(col.rgb, 1);
+}
---
+++ projects/pbrish/shader.glsl
@@ -0,0 +1,37 @@
+#version 300 es
+
+precision highp float;
+
+uniform vec2 iResolution;
+uniform sampler2D iChannel0; // blur-y.glsl filter: linear wrap: clamp
+
+out vec4 fragColorOut;
+
+void mainImage(out vec4 a, in vec2 b);
+
+void main() {
+    mainImage(fragColorOut, gl_FragCoord.xy);
+}
+
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+vec3 aces(vec3 x) {
+  const float a = 2.51;
+  const float b = 0.03;
+  const float c = 2.43;
+  const float d = 0.59;
+  const float e = 0.14;
+  return clamp((x * (a * x + b)) / (x * (c * x + d) + e), 0.0, 1.0);
+}
+
+void mainImage(out vec4 fragColor, in vec2 fragCoord) {
+    vec2 uv = fragCoord.xy / iResolution.xy;
+    vec3 col = texture(iChannel0, uv).rgb;
+    
+    col = aces(col);
+    col = pow( col, vec3(1./2.2) );
+    
+    fragColor = vec4(col, 1);
+}
